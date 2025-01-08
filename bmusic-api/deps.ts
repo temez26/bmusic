98266e1539ebb3c0eedad172e0956e3fb0d9a07b
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-08 11:59:34 +0200

    changed backend architecture to use node and express.js and some udpates on the Dockerfiles on ui and api

--- bmusic-api/deps.ts
+++
@@ -1,2 +0,0 @@
-export { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-export { assertEquals } from "https://deno.land/std@0.114.0/testing/asserts.ts";
--- bmusic-api/main_test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "./deps.ts";
-
-Deno.test(function addTest() {
-  function add(a: number, b: number): number {
-    return a + b;
-  }
-  assertEquals(add(2, 3), 5);
-});
