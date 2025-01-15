a32b96f668bf55eeedbf4d8c849b1bc3f15b2f3b
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-15 11:06:32 +0200

    Refactor app component; remove upload component and implement upload dialog toggle in navbar

--- bmusic-ui/src/app/app.component.ts
+++ bmusic-ui/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { PlayerComponent } from './player/player.component';
 import { SongsComponent } from './songs/songs.component';
-import { UploadComponent } from './upload/upload.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayerComponent, SongsComponent, UploadComponent, NavbarComponent],
+  imports: [PlayerComponent, SongsComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- bmusic-ui/src/app/navbar/navbar.component.ts
+++ bmusic-ui/src/app/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { UploadComponent } from '../upload/upload.component';
 
 @Component({
   selector: 'app-navbar',
@@ -15,9 +18,20 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
   ],
-
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  private dialogRef: MatDialogRef<UploadComponent> | null = null;
+  constructor(private dialog: MatDialog) {}
+
+  toggleUploadDialog(): void {
+    if (this.dialogRef) {
+      this.dialogRef.close();
+      this.dialogRef = null;
+    } else {
+      this.dialogRef = this.dialog.open(UploadComponent);
+    }
+  }
+}
