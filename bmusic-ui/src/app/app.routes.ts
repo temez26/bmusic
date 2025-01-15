7b2ce404371e917de8b1181f4d6fb93136a4b884
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-15 15:37:28 +0200

    Add albums component and update routing; create albums route and adjust links in home component

--- bmusic-ui/src/app/app.routes.ts
+++ bmusic-ui/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SongsComponent } from './songs/songs.component';
+import { AlbumsComponent } from './albums/albums.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'songs', component: SongsComponent },
+  { path: 'albums', component: AlbumsComponent },
 ];
