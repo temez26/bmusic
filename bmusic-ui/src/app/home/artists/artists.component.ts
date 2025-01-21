bf82c1a740ac536636267978206af3bca021616e
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-21 12:50:21 +0200

    Add artists and playlists components with basic structure and tests

---
+++ bmusic-ui/src/app/home/artists/artists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artists',
+  standalone: true,
+  imports: [],
+  templateUrl: './artists.component.html',
+  styleUrl: './artists.component.scss'
+})
+export class ArtistsComponent {
+
+}
