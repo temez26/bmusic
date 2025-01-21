b45b3fe419ba870ec75ff7343d62603170632eec
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-21 12:52:17 +0200

    Add routes for playlists and artists components

--- bmusic-ui/src/app/app.routes.ts
+++ bmusic-ui/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SongsComponent } from './home/songs/songs.component';
 import { AlbumsComponent } from './home/albums/albums.component';
+import { PlaylistsComponent } from './home/playlists/playlists.component';
+import { ArtistsComponent } from './home/artists/artists.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'songs', component: SongsComponent },
   { path: 'albums', component: AlbumsComponent },
+  { path: 'playlists', component: PlaylistsComponent },
+  { path: 'artists', component: ArtistsComponent },
 ];
