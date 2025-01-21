8121987d71e210958157e187f78fb44b56018032
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-21 15:18:19 +0200

    Refactor API service to use a base URL for endpoints and improve error handling

--- bmusic-ui/src/app/service/api.service.ts
+++ bmusic-ui/src/app/service/api.service.ts
@@ -8,10 +8,11 @@ import { Song, UploadResponse } from './models/song-def.class';
   providedIn: 'root',
 })
 export class ApiService {
+  private baseUrl = `http://${window.location.hostname}:4000`;
   constructor(private http: HttpClient) {}
 
   incrementPlayCount(songId: number): Observable<{ playCount: number }> {
-    const url = `http://${window.location.hostname}:4000/increment`;
+    const url = `${this.baseUrl}/increment`;
     return this.http.post<{ playCount: number }>(url, { id: songId }).pipe(
       catchError((error) => {
         console.error('Error incrementing play count:', error);
@@ -21,18 +22,17 @@ export class ApiService {
   }
 
   fetchSongs(): Observable<Song[]> {
-    return this.http
-      .get<Song[]>(`http://${window.location.hostname}:4000/songs`)
-      .pipe(
-        catchError((error) => {
-          console.error('Error fetching songs:', error);
-          return throwError(() => error);
-        })
-      );
+    const url = `${this.baseUrl}/songs`;
+    return this.http.get<Song[]>(url).pipe(
+      catchError((error) => {
+        console.error('Error fetching songs:', error);
+        return throwError(() => error);
+      })
+    );
   }
 
   deleteSong(songId: number): Observable<Song[]> {
-    const url = `http://${window.location.hostname}:4000/delete`;
+    const url = `${this.baseUrl}/delete`;
     return this.http.delete<Song[]>(url, { body: { id: songId } }).pipe(
       catchError((error) => {
         console.error('Error deleting song:', error);
@@ -42,19 +42,15 @@ export class ApiService {
   }
 
   uploadFiles(files: File[]): Observable<UploadResponse> {
+    const url = `${this.baseUrl}/upload`;
     const formData = new FormData();
     files.forEach((file) => formData.append('files', file));
 
-    return this.http
-      .post<UploadResponse>(
-        `http://${window.location.hostname}:4000/upload`,
-        formData
-      )
-      .pipe(
-        catchError((error) => {
-          console.error('Error uploading files:', error);
-          return throwError(() => error);
-        })
-      );
+    return this.http.post<UploadResponse>(url, formData).pipe(
+      catchError((error) => {
+        console.error('Error uploading files:', error);
+        return throwError(() => error);
+      })
+    );
   }
 }
