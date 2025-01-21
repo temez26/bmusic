c4bde3c21a3035d16d71c215bf00fe399c08ec76
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-21 11:25:48 +0200

    Refactor volume icon component; remove unused styles and add volume percentage input

--- bmusic-ui/src/app/player/volume-icon/volume-icon.component.ts
+++ bmusic-ui/src/app/player/volume-icon/volume-icon.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlayerModel } from '../../service/models/player.model';
 
 @Component({
   selector: 'app-volume-icon',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './volume-icon.component.html',
-  styleUrl: './volume-icon.component.scss',
+  styleUrls: ['./volume-icon.component.scss'],
 })
-export class VolumeIconComponent {}
+export class VolumeIconComponent {
+  @Input() volumePercentage: number = 0;
+}
