ae58990bd940a38cd5c805573322ac39ca1d9ea8
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-01-24 09:44:07 +0200

    Refactor songs component: update template to use async pipe and trackBy for improved performance and change detection

--- bmusic-ui/src/app/home/songs/songs.component.ts
+++ bmusic-ui/src/app/home/songs/songs.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { PlayerStateService } from '../../service/player.state.service';
 import { PlayComponent } from '../shared/play/play.component';
 
@@ -9,15 +12,22 @@ import { PlayComponent } from '../shared/play/play.component';
   imports: [CommonModule, PlayComponent],
   templateUrl: './songs.component.html',
   styleUrls: ['./songs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SongsComponent implements OnInit {
-  songs: any[] = [];
+  songs$: Observable<any[]>;
 
-  constructor(private playerStateService: PlayerStateService) {}
+  constructor(private playerStateService: PlayerStateService) {
+    this.songs$ = playerStateService.songs$;
+  }
 
   ngOnInit() {
-    this.playerStateService.songs$.subscribe((songs) => {
-      this.songs = songs.sort((a, b) => a.id - b.id);
-    });
+    this.songs$ = this.playerStateService.songs$.pipe(
+      map((songs) => [...songs].sort((a, b) => a.id - b.id))
+    );
+  }
+
+  trackBySongId(index: number, song: any): number {
+    return song.id;
   }
 }
