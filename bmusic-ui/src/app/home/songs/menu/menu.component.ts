a9c725307c4088763199980d3f2d040d3f225ac0
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-02-03 11:43:23 +0200

    Enhance menu component with click outside detection to improve user experience

--- bmusic-ui/src/app/home/songs/menu/menu.component.ts
+++ bmusic-ui/src/app/home/songs/menu/menu.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener,
+  ElementRef,
+} from '@angular/core';
 import { DeleteComponent } from './delete/delete.component';
 import { CommonModule } from '@angular/common';
 
@@ -7,13 +14,25 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [DeleteComponent, CommonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss',
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
   @Input() isMenuOpen: boolean = false;
   @Output() toggleMenu = new EventEmitter<void>();
 
+  constructor(private elementRef: ElementRef) {}
+
   onToggleMenu() {
     this.toggleMenu.emit();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (
+      this.isMenuOpen &&
+      !this.elementRef.nativeElement.contains(event.target)
+    ) {
+      this.toggleMenu.emit();
+    }
   }
 }
