d10e9863bd2e68776ac23437075688fe5a3cf91c
Teemu Kalmari <95919515+temez26@users.noreply.github.com>
2025-02-12 09:46:46 +0200

    Remove unused import from artist state service for cleaner code

--- bmusic-ui/src/app/service/states/artist.state.service.ts
+++ bmusic-ui/src/app/service/states/artist.state.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, forkJoin } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Artists, CurrentArtist } from '../models/artist.interface';
 
 @Injectable({
